cdf55bb4369f54e0f1a257c14d2ded5e473e9c76
Natalia Gubiani Rampon <ngrampon@inf.ufrgs.br>
2022-04-22 00:38:23 +0000

    test: initial test results for LTP

--- experiments/experiments/scripts/graphics/figure12.sh
+++ experiments/experiments/scripts/graphics/figure12.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Figure 12 - TCP LTP 1kbps 200b payload size
-cat results/scenario12/1k/LTP-TCP-1k-200b.txt | grep sec | head -30 | tr - " " | awk '{print $4, $8}' > ../experiments/scripts/graphics/files/figure12/LTP-TCP-1k-200b.txt
+cat results/scenario3/1k/LTP-TCP-1k-200b.txt | grep sec | head -30 | tr - " " | awk '{print $4, $8}' > ../experiments/scripts/graphics/files/figure12/LTP-TCP-1k-200b.txt
 cd ../experiments/scripts/graphics/files/figure12/
 gnuplot figure12.plot
 cd ../../../../../ltp-proto/
